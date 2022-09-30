7c6b3ef67c8f9f32bfcd335c554048656db8b162
Bazza <barry@bazzaware.co.uk>
2022-09-30 18:41:41 +0100

    Added Health Message

--- Week01/Basic/Input/Data_Types.py
+++ Week01/Basic/Input/Data_Types.py
@@ -11,6 +11,19 @@ class Human():
             (self.weight / (self.heightMeters)**2))
 
 
+def HealthMessage(bmi):
+    if (bmi <= 18.4):
+        print("You are underweight.")
+    elif (bmi <= 24.9):
+        print("You are a healthy weight.")
+    elif (bmi <= 29.9):
+        print("You are overweight.")
+    elif (bmi <= 34.9):
+        print("You are obse.")
+    else:
+        print(f"A BMI value of {bmi} is not valid!")
+
+
 def main():
     os.system('cls')
     name = input("What is your name human?\n>")
@@ -19,8 +32,10 @@ def main():
     weight = float(input("How much do you weigh (in kilograms)?\n>"))
 
     human = Human(name, age, height, weight)
-
-    print(f'{human.name} you are {human.age} years old and your bmi is {human.bmi}')
+    print("\n***************************************************************************\n")
+    print(f'{human.name} you are {human.age} years old and your bmi is {human.bmi}.')
+    HealthMessage(float(human.bmi))
+    print("\n***************************************************************************")
 
 
 if __name__ == "__main__":
