911814931652163406acf7679bc4da0eaefc87cc
Bazza <barry.goodall@os.uk>
2022-09-30 16:11:41 +0100

    New Class No but error handling

---
+++ Week01/Basic/Input/Data_Types.py
@@ -0,0 +1,28 @@
+import os
+
+
+class Human():
+    def __init__(self, name, age, height, weight):
+        self.name = name
+        self.age = age
+        self.heightMeters = height
+        self.weight = weight
+        self.heightCentimeters = height / 100
+        self.bmi = "{:.2f}".format(
+            self.weight / (self.heightCentimeters/100)**2)
+
+
+def main():
+    os.system('cls')
+    name = input("What is your name human?\n>")
+    age = int(input("How old are you (in years)?\n>"))
+    height = float(input("How tall are you (in meters)?\n>"))
+    weight = float(input("How much do you weigh (in kilograms)?\n>"))
+
+    human = Human(name, age, height, weight)
+
+    print(f'{human.name} you are {human.age} years old and your bmi is {human.bmi}')
+
+
+if __name__ == "__main__":
+    main()
