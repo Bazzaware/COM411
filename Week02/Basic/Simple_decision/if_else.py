dfd8c40f0b47689ec26a6a8cf0a186ade85cfd5c
Bazza <6goodb73@solent.ac.uk>
2022-10-04 11:41:25 +0100

    Added code to for if else

---
+++ Week02/Basic/Simple_decision/if_else.py
@@ -0,0 +1,8 @@
+activity = input("Please enter the activity to be performed:\n = ")
+
+if activity == "calculate":
+    print("performing calculations")
+else:
+    print("Performing activity")
+
+print( "Activity completed")
