9bd04f946bc9a8c9641017d24f9f3ea5a0f55446
Bazza <6goodb73@solent.ac.uk>
2022-10-08 19:03:33 +0100

    Isnot an odd number failing test

--- basics/Simple_decision/modulo_operator_test.py
+++ basics/Simple_decision/modulo_operator_test.py
@@ -1,5 +1,8 @@
+from pickle import FALSE
 from modulo_operator import *
 
 
 def test_is_an_odd_number():
     assert is_odd_number(3) == True
+def test_isnot_an_odd_number():
+    assert is_odd_number(2) == False
