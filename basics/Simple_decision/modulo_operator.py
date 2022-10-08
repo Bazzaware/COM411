adcad19b0a050340d94ffe97fa777bc55472e5e2
Bazza <6goodb73@solent.ac.uk>
2022-10-08 20:34:22 +0100

    Added code to demonstrate the use of a modulus operator

--- basics/Simple_decision/modulo_operator.py
+++ basics/Simple_decision/modulo_operator.py
@@ -1,5 +1,5 @@
-from curses.ascii import isdigit
-
+from sys import platform
+from os import system
 
 def is_odd_number(number):
     result = True
@@ -7,3 +7,24 @@ def is_odd_number(number):
         result = False
 
     return result
+
+
+def clear_terminal():
+    if platform == "linux" or platform == "linux2":
+        system("clear")
+    elif platform == "win32":
+        system("cls")
+
+
+def main():
+    clear_terminal()
+    number = int(input("Please enter a whole number.\n "))
+
+    if (is_odd_number(number)):
+        print(f"The number {number} is an odd number.")
+    else:
+        print(f"The number {number} is an even number.")
+
+
+if (__name__ == "__main__"):
+    main()
