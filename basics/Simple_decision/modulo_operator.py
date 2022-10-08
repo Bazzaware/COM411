ff559682d137ee571d8c56cdf51b77ecf92e40a8
Bazza <6goodb73@solent.ac.uk>
2022-10-08 19:34:43 +0100

    Passing tests

--- basics/Simple_decision/modulo_operator.py
+++ basics/Simple_decision/modulo_operator.py
@@ -1,3 +1,9 @@
-def is_odd_number(number):
-    return True
+from curses.ascii import isdigit
 
+
+def is_odd_number(number):
+    result = True
+    if((number % 2) == 0):
+        result = False
+
+    return result
--- basics/Simple_decision/modulo_operator_test.py
+++ basics/Simple_decision/modulo_operator_test.py
@@ -4,5 +4,8 @@ from modulo_operator import *
 
 def test_is_an_odd_number():
     assert is_odd_number(3) == True
+
+
 def test_isnot_an_odd_number():
     assert is_odd_number(2) == False
+
