b9f4343072fedcf120cb574c42a8c067681de20e
Bazza <6goodb73@solent.ac.uk>
2022-10-09 08:40:16 +0100

    Added code to review the use of decisions, nesting and logical operators.

---
+++ basics/decisions/review.py
@@ -0,0 +1,10 @@
+from shared_code import clear_terminal
+import sys
+
+def main():
+    clear_terminal()
+    print("See \\basic\\Output\\ascii-art.py for an example of while and if statements.")
+
+
+if (__name__ == "__main__"):
+    main()
