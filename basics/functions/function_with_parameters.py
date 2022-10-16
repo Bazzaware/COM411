9a1eaba83c5bcf3d6a9e43fa44b1eb09b118fe75
Bazza <6goodb73@solent.ac.uk>
2022-10-16 06:45:57 +0100

    Added code to demonstrate the use of a user-defined function with multiple parameters.

--- basics/functions/function_with_parameters.py
+++ basics/functions/function_with_parameters.py
@@ -1,9 +1,18 @@
-def climb_ladder(stepsRemaining,stepsCrossed):
-    if (stepsRemaining > stepsCrossed):
-        print("Still some way to go!")
+from itertools import count
+
+
+def  escape_by(plan):
+    if (plan.lower() == "jumping over" ):
+        print("We cannot escape that way! The boulder is too big!")
+    elif (plan.lower() == "running around"):
+        print("We cannot escape that way! The boulder is moving too fast!")
+    elif (plan.lower() == "going deeper"):
+        print("That might just work! Let's go deeper!")
     else:
-        print("We are almost there!")
+        print("We cannot escape that way! The boulder is in the way!")
 
-
-climb_ladder(5,2)
-climb_ladder(2,5)
+count = 1
+while (count <= 3):
+    count += 1
+    plan = input("What should we do?\n")
+    escape_by(plan)
