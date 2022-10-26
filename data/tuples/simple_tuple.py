c6a120766a2c010eebe70647288d967a6d3103ee
Bazza <6goodb73@solent.ac.uk>
2022-10-26 20:57:54 +0100

    clear_terminal() works in pycharm

---
+++ data/tuples/simple_tuple.py
@@ -0,0 +1,16 @@
+from basics.MyModules.shared_code import *
+
+def likelihood():
+    likelihoods = (50, 38, 27, 99, 4)
+    return min(likelihoods)
+
+
+def main():
+    clear_terminal()
+    value = likelihood()
+    print(f"Minimum likelihood of falling: {value} %" )
+
+
+if __name__ == "__main__":
+    main()
+
