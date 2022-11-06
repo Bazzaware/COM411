6ce454aa94435c8aa272864b20ec6cdb959314df
Bazza <6goodb73@solent.ac.uk>
2022-11-06 10:00:52 +0000

    Added code to demonstrate a simple set.

---
+++ data/sets/simple_set.py
@@ -0,0 +1,16 @@
+import mymodule as mymod
+
+def observed ():
+    observations = set()
+    observations = {"Flying Car", "Sky Scraper",
+                    "Sky Scraper", "Laser", "Dome", "Dome"}
+    return observations
+
+
+def run():
+    mymod.clear_terminal()
+    print(observed())
+
+
+if __name__ == "__main__":
+    run()
