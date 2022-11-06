23eb58f8f77e7e23008eb871bdc18dfb91dbef1b
Bazza <6goodb73@solent.ac.uk>
2022-11-06 12:42:32 +0000

    Added code to demonstrate how to manipulate and sort a set.

--- data/sets/set_from_list.py
+++ data/sets/set_from_list.py
@@ -13,16 +13,13 @@ def run():
     print(f"Counting observations...")
     observations = observed()
     result = set()
-    output = ""
 
     for observation in observations:
         count = observation, observations.count(observation)
         result.add(count)
-
+    print(f"\nObservations:")
     for item in result:
-        output += f"{item[0]} observed {item[1]} times.\n"
-
-    print(f"\n {output.strip()}")
+        print(f"{item[0]} observed {item[1]} times.")
 
 
 if __name__ == "__main__":
