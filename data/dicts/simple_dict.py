9e219af98118fb216fa2ade8979ea31cada04e03
Bazza <6goodb73@solent.ac.uk>
2022-11-12 07:51:09 +0000

    Added code to demonstrate a simple dictionary.

---
+++ data/dicts/simple_dict.py
@@ -0,0 +1,17 @@
+def pattern():
+    sequences = dict()
+    sequences.update(
+        {
+            'Short Sequence': 3,
+            'Medium Sequence': 2,
+            'Long Sequence': 1
+        })
+    return sequences
+
+
+def run():
+    print(pattern())
+
+
+if __name__ == "__main__":
+    run()
