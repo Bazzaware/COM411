ce93c9c584abf03998e29cf228f243c88b8478e9
Bazza <6goodb73@solent.ac.uk>
2022-11-20 11:04:16 +0000

    CSV not loading

--- data/olympics/main.py
+++ data/olympics/main.py
@@ -1,19 +1,26 @@
 import csv
+from tui import started,menu
 import process
-import tui
 
-
-def read_data(file_path):
-    pass
+def read_data(file_path: str) -> list:
+    '''Reads in data from a CVS file and returns a list with headers removed.'''
+    started(file_path)
+    data = list()
+    with open(file_path, "r", encoding="utf-8") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)
+        for line in csv_reader:
+            data.append(line)
+    return data
 
 
 def run():
     athlete_data = read_data("athlete_events.csv")
 
     while True:
-        selection = tui.menu
+        selection = menu
         if selection == "years":
-            pass
+            process.list_years(athlete_data)
         elif selection == "tally":
             pass
         elif selection == "team":
--- data/olympics/process.py
+++ data/olympics/process.py
@@ -1,18 +1,30 @@
-def list_years(data):
+from tui import started, display_years, completed, display_medal_tally
+
+
+def list_years(data: list) -> None:
     """Returns a list of years from Data"""
+    started("Listing years")
+    years = set()
+    for line in data:
+        year = line[9]
+        years.add(year)
+    display_years(years)
+    completed()
 
 
-def tally_medals(data):
+def tally_medals(data: set) -> None:
     """Returns list of medals"""
+    started("Tallying medals")
+    medal_tally = dict({"Gold": 0, "Silver": 0, "Bronze": 0})
+    for line in data:
+        medal = line[14]
+        if medal in ("Gold", "Silver", "Bronze"):
+            medal_tally[medal] += 1
+    display_medal_tally(medal_tally)
+    completed()
 
 
-def tally_team_medals(data):
+def tally_team_medals(data: set) -> None:
     """Returns nested dictionary"""
-
-
-def run():
-    pass
-
-
-if __name__ == "__main__":
-    run()
+    started("")
+    completed()
