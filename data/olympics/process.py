804a3c545f19a30d5a723eeac5dfc93af19d9468
Bazza <6goodb73@solent.ac.uk>
2022-11-21 15:12:37 +0000

    class now on teams

--- data/olympics/process.py
+++ data/olympics/process.py
@@ -4,8 +4,13 @@ COL_YEAR = 9
 
 
 class Medals():
+    '''Represents a dictionary of medals'''
+
     def __init__(self) -> dict[str, int]:
-        self.medal_tally = dict({"Gold": 0, "Silver": 0, "Bronze": 0})
+        '''Initializes a dictionary with zero values for the medals.
+        dict[str, int]'''
+
+        self.tally = dict({"Gold": 0, "Silver": 0, "Bronze": 0})
 
 
 def list_years(data: list) -> set:
@@ -17,17 +22,17 @@ def list_years(data: list) -> set:
     return years
 
 
-def tally_medals(data: list) -> dict[str, int]:
+def tally_medals(data: list) -> Medals:
     """Returns list of medals"""
     medals = Medals()
     for line in data:
         medal = line[COL_MEDAL]
         if medal in ("Gold", "Silver", "Bronze"):
-            medals.medal_tally[medal] += 1
-    return medals.medal_tally
+            medals.tally[medal] += 1
+    return medals.tally
 
 
-def tally_team_medals(data: list) -> dict[str, dict[str, int]]:
+def tally_team_medals(data: list) -> dict[str, Medals]:
     """Returns nested dictionary"""
     team_medal_tally = {}
 
@@ -40,7 +45,7 @@ def tally_team_medals(data: list) -> dict[str, dict[str, int]]:
         if team_name in team_medal_tally:
             team_medal_tally[team_name][team_medal] += 1
         else:
-            team_medal_tally[team_name] = {"Gold": 0, "Silver": 0, "Bronze": 0}
+            team_medal_tally[team_name] = Medals().tally
             team_medal_tally[team_name][team_medal] += 1
 
     return team_medal_tally
