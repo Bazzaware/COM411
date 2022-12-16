d099a24fea297ebb3efa78a41ff786931fb70569
Bazza <barry@bazzaware.co.uk>
2022-12-16 08:33:09 +0000

    Movning to other computer

--- oop/Data.py
+++ oop/Data.py
@@ -1,4 +1,5 @@
 import csv
+from os.path import exists, splitext
 
 
 class Data():
@@ -8,9 +9,25 @@ class Data():
         self.DATA = self.import_data(file_path)
 
     def import_data(self, file_path) -> None:
-        with open(file_path) as file:
+        if exists(file_path):
+            if file_path.endswith('.csv'):
+                self.import_csv(file_path)
+            elif file_path.endswith('.txt'):
+                self.import_txt(file_path)
+            else:
+                raise TypeError(f'File type not supported: {splitext(file_path)[1]}')
+        else:
+            raise FileNotFoundError(f'File not found:\n\t{file_path}')
+        return self.DATA
+
+    def import_csv(self, import_file):
+        with open(import_file) as file:
             csv_reader = csv.reader(file)
             next(csv_reader)
             for line in csv_reader:
                 self.DATA.append(line)
-        return self.DATA
+
+    def import_txt(self, import_file):
+        with open(import_file) as file:
+            for line in file:
+                self.DATA.append(line)
--- tests/data_test.py
+++ tests/data_test.py
@@ -8,7 +8,7 @@ def test_import_from_csv() -> None:
     Should import data from a csv file
     '''
     # arrange
-    expected = list((
+    expected_csv = list((
         [
             ['1', 'Beep', 'Mercury'],
             ['2', 'Bop', 'Boulder'],
@@ -16,8 +16,53 @@ def test_import_from_csv() -> None:
         ]
     ))
     file_path: str = join('data', 'files', 'csv', 'bots.csv')
+
+    # act
+    result = Data(file_path)
+
+    # assert
+    assert result.DATA == expected_csv
+
+
+def test_import_from_txt() -> None:
+    '''
+    Should import data from a txt file
+    '''
+    # arrange
+    expected = list((
+        'The Law Section\n',
+        'The Art Section\n',
+        'The Technology Section\n',
+        'The History Section'
+    ))
+    file_path: str = join('data', 'files', 'txt', 'library.txt')
+
     # act
     result = Data(file_path)
 
     # assert
     assert result.DATA == expected
+
+
+def test_invalid_file_type() -> None:
+    #  arrange
+    file_path: str = join('data', 'files', 'txt', 'library.bad')
+    expected = 'File type not supported: .bad'
+    #  act
+    with pytest.raises(TypeError) as exp:
+        Data(file_path)
+
+    # assert
+    assert str(exp.value) == expected
+
+
+def test_file_not_exits() -> None:
+    #  arrange
+    file_path: str = join('data', 'files', 'txt', 'fileNotExist.csv')
+    expected = f'File not found:\n\t{file_path}'
+    #  act
+    with pytest.raises(FileNotFoundError) as exp:
+        Data(file_path)
+
+    # assert
+    assert str(exp.value) == expected
